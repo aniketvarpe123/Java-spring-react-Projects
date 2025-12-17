b2889ee45598358f8404291f5a38561084a472eb
Aniket Babasaheb Varpe <110764271+aniketvarpe123@users.noreply.github.com>
2025-12-17 14:22:58 +0530

    Delete billingsoftware directory

--- billingsoftware/src/main/java/in/mgs/billingsoftware/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package in.mgs.billingsoftware.repository;
-
-import in.mgs.billingsoftware.entity.CategoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<CategoryEntity,Long> {
-}
